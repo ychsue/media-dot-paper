d16b6f694075dba2df3e9dffca023f3df8ab0ec2
Young-Chung Hsue <hsuy1234@hotmail.com>
2018-06-11 21:38:53 +0800

    It can copy blob file into videosLibrary of Windows UWP App and that storageFile can be played in video element.

--- cordova-test/www/main.js
+++ cordova-test/www/main.js
@@ -452,7 +452,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<input #selFile type=\"file\" multiple (change)=\"onSelFileChange(selFile.files)\">\r\n<video #audioFromFile autoplay controls [src]=\"audioSrc|safe\">\r\n</video>\r\n<hr>\r\n<div>\r\n    <div>Is cordova support? {{(isCordovaSupport)?\"Yes\":\"No\"}}</div>\r\n    <div>Is cordova.file support? {{(isFilePluginSupport)?\"Yes\":\"No\"}}</div>\r\n</div>"
+module.exports = "<input #selFile type=\"file\" multiple (change)=\"onSelFileChange(selFile.files)\">\r\n<video #audioFromFile autoplay controls [src]=\"audioSrc|safe\">\r\n</video>\r\n<hr>\r\n<div>\r\n    <div>Is cordova support? {{(isCordovaSupport)?\"Yes\":\"No\"}}</div>\r\n    <div>Is cordova.file support? {{(isFilePluginSupport)?\"Yes\":\"No\"}}</div>\r\n</div>\r\n<div>\r\n    <button mat-raised-button (click)=\"onGetDocFolder()\">\r\n        <mat-icon>folder</mat-icon>\r\n    </button>\r\n    {{newFolderName}}\r\n</div>"
 
 /***/ }),
 
@@ -478,6 +478,41 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 var __metadata = (undefined && undefined.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 
 
 
@@ -487,16 +522,70 @@ var TestComponent = /** @class */ (function () {
         this.sanitizer = sanitizer;
     }
     TestComponent.prototype.ngOnInit = function () {
-        this.isCordovaSupport = !!window['cordova'];
+        this.isCordovaSupport = !!window.cordova;
         this.isFilePluginSupport = this.isCordovaSupport && !!cordova.file;
     };
     TestComponent.prototype.onSelFileChange = function (files) {
-        this.audioFile = files[0];
-        // this.audioSrc = this.sanitizer.bypassSecurityTrustUrl('https://www.scripturesongs.net/mp3/h1/01FountainFilledWithBlood.mp3');
-        // this.audioSrc = 'https://www.scripturesongs.net/mp3/h1/01FountainFilledWithBlood.mp3';
-        // this.audioSrc = this.sanitizer.bypassSecurityTrustUrl(window.URL.createObjectURL(this.audioFile));
-        this.audioSrc = window.URL.createObjectURL(this.audioFile);
-        this.msgService.pushMessage({ type: _services_message_service__WEBPACK_IMPORTED_MODULE_1__["MessageTypes"].Info, message: "files: " + JSON.stringify(files[0].name) });
+        return __awaiter(this, void 0, void 0, function () {
+            var fName, outputFile, outStream, inStream, error_1, newFile;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        this.audioFile = files[0];
+                        fName = this.audioFile.name;
+                        if (!(!!window.cordova && cordova.platformId === 'windows')) return [3 /*break*/, 7];
+                        _a.label = 1;
+                    case 1:
+                        _a.trys.push([1, 6, , 7]);
+                        return [4 /*yield*/, Windows.Storage.KnownFolders.videosLibrary.createFileAsync(this.audioFile.name)];
+                    case 2:
+                        outputFile = _a.sent();
+                        return [4 /*yield*/, outputFile.openAsync(Windows.Storage.FileAccessMode.readWrite)];
+                    case 3:
+                        outStream = _a.sent();
+                        inStream = this.audioFile.msDetachStream();
+                        return [4 /*yield*/, Windows.Storage.Streams.RandomAccessStream.copyAsync(inStream, outStream)];
+                    case 4:
+                        _a.sent();
+                        return [4 /*yield*/, outStream.flushAsync()];
+                    case 5:
+                        _a.sent();
+                        inStream.close();
+                        outStream.close();
+                        return [3 /*break*/, 7];
+                    case 6:
+                        error_1 = _a.sent();
+                        this.msgService.pushMessage({ type: _services_message_service__WEBPACK_IMPORTED_MODULE_1__["MessageTypes"].Error, message: error_1 });
+                        return [3 /*break*/, 7];
+                    case 7: return [4 /*yield*/, Windows.Storage.KnownFolders.videosLibrary.getFileAsync(fName)];
+                    case 8:
+                        newFile = _a.sent();
+                        this.audioSrc = window.URL.createObjectURL(newFile);
+                        this.msgService.pushMessage({ type: _services_message_service__WEBPACK_IMPORTED_MODULE_1__["MessageTypes"].Info, message: "audioSrc: " + JSON.stringify(this.audioSrc) });
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    TestComponent.prototype.onGetDocFolder = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var docFolder, newFolder;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        if (!!!window['Windows']) return [3 /*break*/, 2];
+                        docFolder = Windows.Storage.KnownFolders.videosLibrary;
+                        return [4 /*yield*/, docFolder.createFolderAsync('Test')];
+                    case 1:
+                        newFolder = _a.sent();
+                        if (newFolder != null) {
+                            this.newFolderName = newFolder.path;
+                        }
+                        _a.label = 2;
+                    case 2: return [2 /*return*/];
+                }
+            });
+        });
     };
     TestComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -14,6 +14,7 @@ export class TestComponent implements OnInit {
   audioSrc: string;
   isCordovaSupport: boolean;
   isFilePluginSupport: boolean;
+  newFolderName: string;
 
   constructor(private msgService: MessageService, private sanitizer: DomSanitizer) { }
 
@@ -22,12 +23,37 @@ export class TestComponent implements OnInit {
     this.isFilePluginSupport = this.isCordovaSupport && !!cordova.file;
   }
 
-  onSelFileChange(files: FileList) {
+  async onSelFileChange(files: FileList) {
     this.audioFile = files[0];
+    const fName = this.audioFile.name;
+    if (!!window.cordova && cordova.platformId === 'windows') {
+      try {
+        const outputFile = await Windows.Storage.KnownFolders.videosLibrary.createFileAsync(this.audioFile.name);
+        const outStream = await outputFile.openAsync(Windows.Storage.FileAccessMode.readWrite);
+        const inStream = this.audioFile.msDetachStream();
+        await Windows.Storage.Streams.RandomAccessStream.copyAsync(inStream, outStream);
+        await outStream.flushAsync();
+        inStream.close();
+        outStream.close();
+      } catch (error) {
+        this.msgService.pushMessage({type: MessageTypes.Error, message: error});
+      }
+    }
     // this.audioSrc = this.sanitizer.bypassSecurityTrustUrl('https://www.scripturesongs.net/mp3/h1/01FountainFilledWithBlood.mp3');
     // this.audioSrc = 'https://www.scripturesongs.net/mp3/h1/01FountainFilledWithBlood.mp3';
     // this.audioSrc = this.sanitizer.bypassSecurityTrustUrl(window.URL.createObjectURL(this.audioFile));
-    this.audioSrc = window.URL.createObjectURL(this.audioFile);
+    const newFile = await Windows.Storage.KnownFolders.videosLibrary.getFileAsync(fName);
+    this.audioSrc = window.URL.createObjectURL(newFile);
     this.msgService.pushMessage({type : MessageTypes.Info, message: `audioSrc: ${JSON.stringify(this.audioSrc)}`});
+  }
+
+  async onGetDocFolder() {
+    if (!!window['Windows']) {
+      const docFolder = Windows.Storage.KnownFolders.videosLibrary;
+      const newFolder = await docFolder.createFolderAsync('Test');
+      if (newFolder != null) {
+        this.newFolderName = newFolder.path;
+      }
+    }
   }
 }
