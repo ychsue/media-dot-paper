475c7c4e550ebbe5f81d4d7000951e3cfe4bd18f
Young-Chung Hsue <hsuy1234@hotmail.com>
2018-09-13 18:07:26 +0800

    For gradle-4.4,  by replacing 4.1 to 4.4

--- cordova-test/platforms/android/app/build/generated/source/buildConfig/debug/tw/at/yescirculation/mediadotpaper/BuildConfig.java
+++ cordova-test/platforms/android/app/build/generated/source/buildConfig/debug/tw/at/yescirculation/mediadotpaper/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "tw.at.yescirculation.mediadotpaper";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 10000;
-  public static final String VERSION_NAME = "1.0.0";
+  public static final int VERSION_CODE = 20271201;
+  public static final String VERSION_NAME = "2018.912.1";
 }
