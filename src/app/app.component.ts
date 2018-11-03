fb51d636da39781ead5a4a2a9d3e5a270511dd43
Young-Chung Hsue <hsuy1234@hotmail.com>
2018-11-03 15:38:53 +0800

    Update app, navbar and welcome components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { IProgressPara, InProgressComponent } from './components/in-progress/in-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './common-use.css']
 })
 export class AppComponent implements AfterViewInit {
 
