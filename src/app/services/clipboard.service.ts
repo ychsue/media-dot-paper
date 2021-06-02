4528d459a7d13f7cbae084fd54a03a3f6280c1a8
Young-Chung Hsue <hsuy1234@hotmail.com>
2021-06-02 14:25:35 +0800

    Correct clipboard part for windows system

--- src/app/services/clipboard.service.ts
+++ src/app/services/clipboard.service.ts
@@ -15,6 +15,28 @@ export class ClipboardService {
     return new Promise<string>((res, rej) => {
       if (!!window?.cordova?.plugins && cordova.platformId !== 'browser' && !!cordova.plugins.clipboard) {
         cordova.plugins.clipboard.paste(t => res(t), e => rej(e));
+      } else if(!!window?.Windows) {
+        try {
+          var text = "";
+          var dataPackageView =
+            Windows.ApplicationModel.DataTransfer.Clipboard.getContent();
+          if (
+            dataPackageView.contains(
+              Windows.ApplicationModel.DataTransfer.StandardDataFormats
+                .text
+            )
+          ) {
+            dataPackageView.getTextAsync().then(function (value) {
+              text = value;
+              res(text);
+            });
+          } else {
+            throw new Error("not a txt in clipboard");
+          }
+        } catch (e) {
+          rej(e);
+        }
+
       } else if (!!window?.navigator?.clipboard) {
         window.navigator.clipboard.readText()
           .then(t => { res(t) })
